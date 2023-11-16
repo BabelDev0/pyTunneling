6cdab0cc23ca743c67e0a480a9000469bc8f535f
BabelDev0 <leonardo.straccali@gmail.com>
2023-11-16 11:06:40 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+class Prova:
+    def __init__(self) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "Prova"
+
+def main():
+    print("Hello World!")
+    print(Prova())
+
+
+if __name__ == '__main__':
+    main()
